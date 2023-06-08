3fd343308f8c17ce5b886993dd34b52bc1c345d7
dokimion24 <leecy3610@gmail.com>
2023-06-08 16:27:08 +0900

    feat: :sparkles: 프로젝트 초기 설정

---
+++ src/common/Header.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header className="flex fixed justify-between items-center z-10 w-[calc(100%-24.4rem)] h-[7.2rem] bg-bg-1 border-b border-solid border-transparent-navy-30 py-[1.1rem] px-[2.3rem]">
+      ddd
+    </header>
+  )
+}
