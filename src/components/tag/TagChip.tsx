c85a17bd0230b55218d298bdb945a511068293e3
youngeun <jessicak0828@gmail.com>
2023-06-24 23:27:29 +0900

    feat: Tag 삭제하는 로직 만들기

--- src/components/tag/TagChip.tsx
+++ src/components/tag/TagChip.tsx
@@ -1,15 +1,21 @@
-import React, { ReactNode } from 'react'
+import React, { useRef,ReactNode } from 'react'
 
 interface TagChipProps {
-  children: ReactNode
-
+  children: ReactNode,
+  arr:string[],
+  setArr: Function
 }
 
-export default function TagChip({children}:TagChipProps) {
+export default function TagChip({children,arr, setArr}:TagChipProps) {
+  const tagRef = useRef()
+  const deleteTagHandler = () => {
+    const newArr = arr.filter((item)=>item !== tagRef.current.innerHTML) 
+    setArr(newArr)  
+  }
   return (
     <div className='flex gap-[1rem] items-center h-[3.6rem] mt-[2rem] py-[0.8rem] px-[1.6rem] rounded-[1.8rem] bg-neutral-navy-300 text-neutral-white-50 hover:cursor-pointer'>
-      <span>{children}</span>
-      <span>X</span>
+      <span ref={tagRef}>{children}</span>
+      <span className='hover:cursor-pointer' onClick={deleteTagHandler}>X</span>
     </div>
   )
 }
--- src/components/tag/TagsContainer.tsx
+++ src/components/tag/TagsContainer.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 import Tag from './TagChip'
+
+interface TagsContainerProps {
+  tagList?: string[]
+}
 
 const arr = ['smiling', 'man', 'waving', 'hands', 'doubled', 'handed']
 
-export default function TagsContainer() {
+export default function TagsContainer({tagList=[]}:TagsContainerProps) {
+  const [arr, setArr] = useState(tagList)
   //FIXME - keyProp 바꾸기
   return (
     <div className='flex flex-wrap gap-[0.8rem]'>
-      {arr.map((item)=><Tag key={item}>{item}</Tag>)}
+      {tagList.length && arr.map((item)=><Tag key={item} arr={arr} setArr={setArr}>{item}</Tag>)}
     </div>
   )
 }
