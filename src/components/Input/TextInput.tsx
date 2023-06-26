3c467e14963a1411290ebbd1d075c5faecff4f08
youngeun <jessicak0828@gmail.com>
2023-06-26 10:05:46 +0900

    feat: TextInput 컴포넌트 counter 기능 구현

--- src/components/Input/TextInput.tsx
+++ src/components/Input/TextInput.tsx
@@ -1,19 +1,27 @@
 "use client"
-import React, {useRef} from 'react'
+import React, {useState,useRef} from 'react'
 
 interface TextInputProps {
   type?: 'input' | 'textarea'
   width: number,
   height?:number,
-  placeholder: string, 
+  placeholder: string,
+  inputValue?:string, 
   counter?: number,
+  disabled?:boolean,
+  assetdata?:object,
+  setAssetdata?: Function
   
 }
 
-export default function TextInput({type='input' ,width, height=4.2, placeholder, counter}:TextInputProps) {
-  const inputRef = useRef();
+export default function TextInput({type='input' ,width, height=4.2, placeholder, inputValue,counter, disabled=false, assetdata, setAssetdata}:TextInputProps) {
+  const inputRef = useRef(null);
+  const [inputLength, setInputLength] = useState(inputValue?.length)
   const submitHandler = (e:SubmitEvent) => {
     e.preventDefault()
+  }
+  const countInputHandler = (e) => {
+    setInputLength(e.target.value.length)
   }
   // const textLength =  inputRef.current.value.length 
 
@@ -21,9 +29,9 @@ export default function TextInput({type='input' ,width, height=4.2, placeholder,
     <>
     {type === 'input' ? (
         <form onSubmit={submitHandler}>
-          <input style={{width: `${width}rem`, height:`${height}rem`}} className='py-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem]' ref={inputRef} type="text" placeholder={placeholder}/>
+          <input style={{width: `${width}rem`, height:`${height}rem`}} className='py-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem] focus:border-neutral-navy-200' ref={inputRef} placeholder={inputValue ? inputValue : placeholder}  disabled={disabled} onChange={counter && countInputHandler}/>
           <div className='relative'>
-            {counter && <p className='text-[1.4rem] font-light absolute -bottom-[3rem] left-[53.5rem]'><span className='text-primary-newlloyd-button '>{0}</span><span>  / {counter}</span></p>}
+            {counter && <p className='text-[1.4rem] font-light absolute -bottom-[3rem] left-[53.5rem]'><span className='text-primary-newlloyd-button '>{inputLength}</span><span>  / {counter}</span></p>}
           </div>
         </form>)
         :
