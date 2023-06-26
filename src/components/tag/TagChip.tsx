17f14cc5315606ca319b3acab5a6a2c6d37ef116
youngeun <jessicak0828@gmail.com>
2023-06-26 10:01:49 +0900

    refactor: Tag, TagContainer 컴포넌트 리팩토링 - useRef 초기값 null 주기

--- src/components/tag/TagChip.tsx
+++ src/components/tag/TagChip.tsx
@@ -7,7 +7,7 @@ interface TagChipProps {
 }
 
 export default function TagChip({children,arr, setArr}:TagChipProps) {
-  const tagRef = useRef()
+  const tagRef = useRef(null)
   const deleteTagHandler = () => {
     const newArr = arr.filter((item)=>item !== tagRef.current.innerHTML) 
     setArr(newArr)  
@@ -15,7 +15,7 @@ export default function TagChip({children,arr, setArr}:TagChipProps) {
   return (
     <div className='flex gap-[1rem] items-center h-[3.6rem] mt-[2rem] py-[0.8rem] px-[1.6rem] rounded-[1.8rem] bg-neutral-navy-300 text-neutral-white-50 hover:cursor-pointer'>
       <span ref={tagRef}>{children}</span>
-      <span className='hover:cursor-pointer' onClick={deleteTagHandler}>X</span>
+      <span className='hover:cursor-pointer px-[1rem]' onClick={deleteTagHandler}>X</span>
     </div>
   )
 }
--- src/components/tag/TagsContainer.tsx
+++ src/components/tag/TagsContainer.tsx
@@ -1,19 +1,17 @@
 "use client"
 import React, {useState} from 'react'
-import Tag from './TagChip'
+import TagChip from './TagChip'
 
 interface TagsContainerProps {
   tagList?: string[]
 }
 
-const arr = ['smiling', 'man', 'waving', 'hands', 'doubled', 'handed']
 
 export default function TagsContainer({tagList=[]}:TagsContainerProps) {
   const [arr, setArr] = useState(tagList)
-  //FIXME - keyProp 바꾸기
   return (
     <div className='flex flex-wrap gap-[0.8rem]'>
-      {tagList.length && arr.map((item)=><Tag key={item} arr={arr} setArr={setArr}>{item}</Tag>)}
+      { arr.map((item)=><TagChip key={item} arr={arr} setArr={setArr}>{item}</TagChip>)}
     </div>
   )
 }
