3931cd207bb55003332571f1b74f072b1a218d2e
youngeun <jessicak0828@gmail.com>
2023-06-28 04:52:21 +0900

    refactor: 이벤트핸들러 변경

--- src/components/Input/TextInput.tsx
+++ src/components/Input/TextInput.tsx
@@ -9,40 +9,37 @@ interface TextInputProps {
   inputValue?:string, 
   counter?: number,
   disabled?:boolean,
-  assetdata?:object,
-  setAssetdata?: Function
+  doesEnterWork?: boolean 
   
 }
 
-export default function TextInput({type='input' ,width, height=4.2, placeholder, inputValue,counter, disabled=false, assetdata, setAssetdata}:TextInputProps) {
+export default function TextInput({type='input' ,width, height=4.2, placeholder, inputValue,counter, disabled=false, doesEnterWork=false}:TextInputProps) {
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const [inputLength, setInputLength] = useState(inputValue?.length)
+  const [inputLength, setInputLength] = useState<number | undefined>(inputValue?.length)
 
-  const handlerSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-  }
-  const handlerCountInput = (e:ChangeEvent<HTMLInputElement>) => {
+  const handlerKeyDown = (e:KeyboardEvent) => {
+    if(e.key==='Enter'){
+      console.log('form')
+    } else {
     setInputLength(e.target.value.length)
+    }
   }
-  // const textLength =  inputRef.current.value.length 
 
   return (
     <>
     {type === 'input' ? (
-        <form onSubmit={handlerSubmit}>
-          <input style={{width: `${width}rem`, height:`${height}rem`}} className='py-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem] focus:border-neutral-navy-200' ref={inputRef} placeholder={inputValue ? inputValue : placeholder}  disabled={disabled} onChange={handlerCountInput}/>
+        <div>
+          <input style={{width: `${width}rem`, height:`${height}rem`}} className={'py-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem] focus:border-neutral-navy-200 disabled:bg-neutral-navy-850' } ref={inputRef} placeholder={inputValue ? inputValue : placeholder}  disabled={disabled} onKeyDown={handlerKeyDown}/>
           <div className='relative'>
             {counter && <p className='text-[1.4rem] font-light absolute -bottom-[3rem] left-[53.5rem]'><span className='text-primary-newlloyd-button '>{inputLength}</span><span>  / {counter}</span></p>}
-
           </div>
-        </form>
-      ) : (
-        <textarea
-          style={{ width: `${width}rem`, height: `${height}rem` }}
-          className="p-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem]"
-          placeholder={placeholder}
-        ></textarea>
-      )}
+        </div>
+        )
+        :
+        (
+        <textarea style={{width: `${width}rem`, height:`${height}rem`}} className='p-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem]' placeholder={placeholder}></textarea>
+        )
+    }
     </>
   )
 }
