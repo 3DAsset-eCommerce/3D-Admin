9890a8774baaa4f245b7b6fbb57f1e9644d559b1
youngeun <jessicak0828@gmail.com>
2023-07-02 02:42:01 +0900

    feat: Input 컴포넌트에 tags 관련 기능 추가하기

--- src/components/Input/TextInput.tsx
+++ src/components/Input/TextInput.tsx
@@ -1,45 +1,72 @@
-"use client"
-import React, {useState,useRef, FormEvent, ChangeEvent} from 'react'
+'use client'
+import React, { useState, useRef, KeyboardEvent } from 'react'
 
 interface TextInputProps {
   type?: 'input' | 'textarea'
-  width: number,
-  height?:number,
-  placeholder: string,
-  inputValue?:string, 
-  counter?: number,
-  disabled?:boolean,
-  doesEnterWork?: boolean 
-  
+  width: number
+  height?: number
+  placeholder: string
+  inputValue?: string
+  counter?: boolean
+  disabled?: boolean
+  tagsArr: string[]
+  setTagsArr: Function
 }
 
-export default function TextInput({type='input' ,width, height=4.2, placeholder, inputValue,counter, disabled=false, doesEnterWork=false}:TextInputProps) {
-  const inputRef = useRef<HTMLInputElement | null>(null);
+export default function TextInput({
+  type = 'input',
+  width,
+  height = 4.2,
+  placeholder,
+  inputValue,
+  counter,
+  disabled = false,
+  ...props
+}: TextInputProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null)
   const [inputLength, setInputLength] = useState<number | undefined>(inputValue?.length)
+  const tagsArr = props.tagsArr
+  const setTagsArr = props.setTagsArr
 
-  const handlerKeyDown = (e:KeyboardEvent) => {
-    if(e.key==='Enter'){
-      console.log('form')
+  const handlerKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setTagsArr([...tagsArr, e.target.value])
+      e.target.value = ''
     } else {
-    setInputLength(e.target.value.length)
+      setInputLength(e.target.value.length)
     }
   }
 
   return (
     <>
-    {type === 'input' ? (
+      {type === 'input' ? (
         <div>
-          <input style={{width: `${width}rem`, height:`${height}rem`}} className={'py-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem] focus:border-neutral-navy-200 disabled:bg-neutral-navy-850' } ref={inputRef} placeholder={inputValue ? inputValue : placeholder}  disabled={disabled} onKeyDown={handlerKeyDown}/>
-          <div className='relative'>
-            {counter && <p className='text-[1.4rem] font-light absolute -bottom-[3rem] left-[53.5rem]'><span className='text-primary-newlloyd-button '>{inputLength}</span><span>  / {counter}</span></p>}
+          <input
+            style={{ width: `${width}rem`, height: `${height}rem` }}
+            className={
+              'rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] py-[0.9rem] text-[1.4rem] focus:border-neutral-navy-200 disabled:bg-neutral-navy-850'
+            }
+            ref={inputRef}
+            placeholder={inputValue ? inputValue : placeholder}
+            disabled={disabled}
+            onKeyDown={handlerKeyDown}
+          />
+          <div className="relative">
+            {counter && (
+              <p className="absolute -bottom-[3rem] left-[53.5rem] text-[1.4rem] font-light">
+                <span className="text-primary-newlloyd-button ">{inputLength}</span>
+                <span> / {counter}</span>
+              </p>
+            )}
           </div>
         </div>
-        )
-        :
-        (
-        <textarea style={{width: `${width}rem`, height:`${height}rem`}} className='p-[0.9rem] rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] text-[1.4rem]' placeholder={placeholder}></textarea>
-        )
-    }
+      ) : (
+        <textarea
+          style={{ width: `${width}rem`, height: `${height}rem` }}
+          className="rounded border border-[#474E57] bg-neutral-navy-950 p-[0.9rem] px-[2rem] text-[1.4rem]"
+          placeholder={placeholder}
+        ></textarea>
+      )}
     </>
   )
 }
