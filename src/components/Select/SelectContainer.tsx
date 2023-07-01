cae2886aa05546e1073fe37791317c8cc25d0aee
youngeun <jessicak0828@gmail.com>
2023-07-02 02:42:40 +0900

    feat: 선택한 카테고리 실시간으로 보이기

--- src/components/Select/SelectContainer.tsx
+++ src/components/Select/SelectContainer.tsx
@@ -24,16 +24,16 @@ export default function SelectContainer() {
   const handleOptionClick = (e) => {
     console.log(e.target.textContent)
   if(e.target.id = 'mainCategory'){
-    setSelectedOption((prev) => ({
-      ...prev,
-      ['main']: e.target.textContent,
-    }))
-  } else {
-    setSelectedOption((prev) => ({
-      ...prev,
-      ['sub']: e.target.textContent,
-    }))
-  }
+      setSelectedOption((prev) => ({
+        ...prev,
+        ['main']: e.target.textContent,
+      }))
+    } else {
+      setSelectedOption((prev) => ({
+        ...prev,
+        ['sub']: e.target.textContent,
+      }))
+    }
   }
 
   useEffect(() => {
@@ -46,66 +46,75 @@ export default function SelectContainer() {
   }, [])
 
   return (
-    <div className="flex gap-[1.6rem]">
-      <Listbox
-        // value={selectedList}
-        // onChange={setSelectedList}
-        by="id"
-        as="div"
-        className="relative flex flex-col gap-0"
-      >
-        <Listbox.Button id='mainCategory' className="flex min-h-[4.2rem] min-w-[24rem] justify-between rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] py-[0.9rem] text-[1.4rem] focus:border-neutral-navy-200">
-          <span>{selectedOption.main}</span>
-          <DropDownIcon />
-        </Listbox.Button>
-        <Listbox.Options className="absolute top-[4.2rem] z-10 rounded border border-[#474E57] bg-neutral-navy-950 text-[1.4rem]">
-          {categoryList.map((item) => (
-            <Listbox.Option
-              key={item.id}
-              value={item}
-              className="flex min-h-[3.5rem] min-w-[24rem] items-center border border-[rgba(0,0,0,0)] px-[2rem] py-[0.9rem] hover:border-neutral-navy-200"
-              onClick={handleOptionClick}
-            >
-              {item.categoryName}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </Listbox>
-      <Listbox
-        // value={selectedList}
-        // onChange={setSelectedList}
-        by="id"
-        as="div"
-        className="relative flex flex-col gap-0 "
-        disabled={disabled}
-      >
-        <Listbox.Button
+    <>
+      <div className="flex gap-[1.6rem]">
+        <Listbox
+          // value={selectedList}
+          // onChange={setSelectedList}
+          by="id"
           as="div"
-          id='subCategory'
-          className={
-            disabled
-              ? 'flex min-h-[4.2rem] min-w-[24rem] justify-between rounded border border-[#474E57] bg-neutral-navy-850 px-[2rem] py-[0.9rem] text-[1.4rem] focus:border-neutral-navy-200'
-              : 'flex min-h-[4.2rem] min-w-[24rem] justify-between rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] py-[0.9rem] text-[1.4rem] focus:border-neutral-navy-200'
-          }
+          className="relative flex flex-col gap-0"
         >
-          <span>{selectedOption.sub}</span>
-          <DropDownIcon />
-        </Listbox.Button>
-        {!disabled && (
-          <Listbox.Options className="absolute top-[4.2rem] rounded border border-[#474E57] bg-neutral-navy-950 text-[1.4rem]">
-            {subCategoryList?.map((item) => (
+          <Listbox.Button
+            id="mainCategory"
+            className="flex min-h-[4.2rem] min-w-[24rem] justify-between rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] py-[0.9rem] text-[1.4rem] focus:border-neutral-navy-200"
+          >
+            <span>{selectedOption.main}</span>
+            <DropDownIcon />
+          </Listbox.Button>
+          <Listbox.Options className="absolute top-[4.2rem] z-10 rounded border border-[#474E57] bg-neutral-navy-950 text-[1.4rem]">
+            {categoryList.map((item) => (
               <Listbox.Option
                 key={item.id}
                 value={item}
-                onClick={handleOptionClick}
                 className="flex min-h-[3.5rem] min-w-[24rem] items-center border border-[rgba(0,0,0,0)] px-[2rem] py-[0.9rem] hover:border-neutral-navy-200"
+                onClick={handleOptionClick}
               >
                 {item.categoryName}
               </Listbox.Option>
             ))}
           </Listbox.Options>
-        )}
-      </Listbox>
-    </div>
+        </Listbox>
+        <Listbox
+          // value={selectedList}
+          // onChange={setSelectedList}
+          by="id"
+          as="div"
+          className="relative flex flex-col gap-0 "
+          disabled={disabled}
+        >
+          <Listbox.Button
+            as="div"
+            id="subCategory"
+            className={
+              disabled
+                ? 'flex min-h-[4.2rem] min-w-[24rem] justify-between rounded border border-[#474E57] bg-neutral-navy-850 px-[2rem] py-[0.9rem] text-[1.4rem] focus:border-neutral-navy-200'
+                : 'flex min-h-[4.2rem] min-w-[24rem] justify-between rounded border border-[#474E57] bg-neutral-navy-950 px-[2rem] py-[0.9rem] text-[1.4rem] focus:border-neutral-navy-200'
+            }
+          >
+            <span>{selectedOption.sub}</span>
+            <DropDownIcon />
+          </Listbox.Button>
+          {!disabled && (
+            <Listbox.Options className="absolute top-[4.2rem] rounded border border-[#474E57] bg-neutral-navy-950 text-[1.4rem]">
+              {subCategoryList?.map((item) => (
+                <Listbox.Option
+                  key={item.id}
+                  value={item}
+                  onClick={handleOptionClick}
+                  className="flex min-h-[3.5rem] min-w-[24rem] items-center border border-[rgba(0,0,0,0)] px-[2rem] py-[0.9rem] hover:border-neutral-navy-200"
+                >
+                  {item.categoryName}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          )}
+        </Listbox>
+      </div>
+      <p className="absolute mt-[0.8rem] text-mm">
+        선택한 카테고리: {selectedOption.main} {' > '}
+        {selectedOption.sub}
+      </p>
+    </>
   )
 }
