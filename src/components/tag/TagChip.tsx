5e9d3f8ddaa9365f3b60f16c0f36d9eee241a809
youngeun <jessicak0828@gmail.com>
2023-07-02 02:41:30 +0900

    feat: Tags Input 기능 구현

--- src/components/tag/TagChip.tsx
+++ src/components/tag/TagChip.tsx
@@ -1,21 +1,24 @@
-import React, { useRef,ReactNode } from 'react'
+'use client'
+import React, { useRef, ReactNode } from 'react'
 
 interface TagChipProps {
-  children: ReactNode,
-  arr:string[],
-  setArr: Function
+  children: ReactNode
+  tagsArr: string[]
+  setTagsArr: Function
 }
 
-export default function TagChip({children,arr, setArr}:TagChipProps) {
+export default function TagChip({ children, tagsArr, setTagsArr }: TagChipProps) {
   const tagRef = useRef<HTMLSpanElement | null>(null)
   const deleteTagHandler = () => {
-    const newArr = arr.filter((item)=>item !== tagRef.current.innerHTML) 
-    setArr(newArr)  
+    const newArr = tagsArr.filter((item) => item !== tagRef.current.innerHTML)
+    setTagsArr(newArr)
   }
   return (
-    <div className='flex gap-[1rem] items-center h-[3.6rem] mt-[2rem] py-[0.8rem] px-[1.6rem] rounded-[1.8rem] bg-neutral-navy-300 text-neutral-white-50 hover:cursor-pointer'>
+    <div className="mt-[2rem] flex h-[3.6rem] items-center gap-[1rem] rounded-[1.8rem] bg-neutral-navy-300 px-[1.6rem] py-[0.8rem] text-neutral-white-50 hover:cursor-pointer">
       <span ref={tagRef}>{children}</span>
-      <span className='hover:cursor-pointer px-[1rem]' onClick={deleteTagHandler}>X</span>
+      <span className="px-[1rem] hover:cursor-pointer" onClick={deleteTagHandler}>
+        X
+      </span>
     </div>
   )
 }
--- src/components/tag/TagsContainer.tsx
+++ src/components/tag/TagsContainer.tsx
@@ -1,17 +1,30 @@
-"use client"
-import React, {useState} from 'react'
+'use client'
+import React, { useState } from 'react'
+import TextInput from '../Input/TextInput'
 import TagChip from './TagChip'
 
 interface TagsContainerProps {
   tagList?: string[]
 }
 
-
-export default function TagsContainer({tagList=[]}:TagsContainerProps) {
-  const [arr, setArr] = useState(tagList)
+export default function TagsContainer({ tagList = [] }: TagsContainerProps) {
+  const [tagsArr, setTagsArr] = useState(tagList)
   return (
-    <div className='flex flex-wrap gap-[0.8rem]'>
-      { arr.map((item)=><TagChip key={item} arr={arr} setArr={setArr}>{item}</TagChip>)}
-    </div>
+    <>
+      <TextInput
+        width={38.8}
+        placeholder="태그를 입력해주세요. (최대 10개)"
+        tagsArr={tagsArr}
+        setTagsArr={setTagsArr}
+      />
+      <div className="flex flex-wrap gap-[0.8rem]">
+        {tagsArr.length !== 0 &&
+          tagsArr.map((item) => (
+            <TagChip key={item} tagsArr={tagsArr} setTagsArr={setTagsArr}>
+              {item}
+            </TagChip>
+          ))}
+      </div>
+    </>
   )
 }
