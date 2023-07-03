391570b121e37a0a537b3b11f39867f7f4b402ed
ronieo <romelzen50@gmail.com>
2023-07-03 17:19:53 +0900

    feat: :sparkles: client component 분기 및 소스트리 수정
    - SideNav, 에셋등록 수정, login, logout 컴포넌트 추가

---
+++ src/components/Common/Layout/index.tsx
@@ -0,0 +1,13 @@
+import SideNav from '@/components/SideNav'
+import React, { ReactNode } from 'react'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <SideNav />
+      <div className="ml-[24.4rem] h-full w-full">
+        <main className="mt-[7.2rem] min-h-[calc(100vh-7.2rem)] bg-bg-2">{children}</main>
+      </div>
+    </div>
+  )
+}
