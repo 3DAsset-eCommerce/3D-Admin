fc2962b5bdce3431696644d0be46f315f97ca0ae
youngeun <jessicak0828@gmail.com>
2023-07-04 02:01:46 +0900

    feat: 파일 업로드 기능 구현

--- src/components/Input/FileUploader.tsx
+++ src/components/Input/FileUploader.tsx
@@ -1,33 +1,57 @@
 'use client'
-import React, {ReactNode, useState ,useRef, ChangeEvent} from 'react'
+import React, { ReactNode, useState, ChangeEvent } from 'react'
+import { useDispatch } from 'react-redux'
+import { createFileUrl, createFileExtension, createFileSize } from '@/store/assetSlice'
 import Button from '../Button'
 
 interface FileUploaderProps {
-  inputWidth: number,
-  inputHeight?: number,
-  placeholder?: string,
-  inputValue?:string, //file url
+  inputWidth: number
+  inputHeight?: number
+  placeholder?: string
+  inputValue?: string //file url
 
-  
-  children?: ReactNode,
+  children?: ReactNode
 }
 
-export default function FileUploader({children,inputWidth, inputHeight=12, inputValue,...props}:FileUploaderProps) {
-
- 
-  const inputRef = useRef<HTMLInputElement | null>(null)
-  const [value, setValue] = useState(inputValue?inputValue:'선택된 파일 없음')
-  const uploadFileHandler = (e: ChangeEvent) => {
-    e.preventDefault()
-    // console.log(inputRef.current.files)
-    setValue(inputRef?.current.files[0].name)
+export default function FileUploader({
+  children,
+  inputWidth,
+  inputHeight = 12,
+  inputValue,
+  ...props
+}: FileUploaderProps) {
+  const [value, setValue] = useState(inputValue ? inputValue : '선택된 파일 없음')
+  const dispatch = useDispatch()
+  const createFileInfo = (file: File) => {
+    const fileSize = Number((file.size / 1024).toFixed(2))
+    dispatch(createFileUrl(file))
+    // dispatch(createFileExtension())
+    // dispatch(createFileSize(fileSize))
   }
+  const uploadFileHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    if (e.currentTarget.files !== null) {
+      console.log('file:', e.currentTarget.files[0])
+      setValue(e.currentTarget.files[0].name)
+      // setTimeout(createFileInfo(e.currentTarget.files[0]), 5000)
+    } else {
+      alert('파일이 존재하지 않습니다.')
+    }
+  }
+
   return (
-        <div className='relative flex gap-[0.8rem]'>
-          <input disabled style={{width: `${inputWidth}rem`, height: `${inputHeight}rem`}} className='rounded border border-[#474E57] bg-neutral-navy-950 px-[1.1rem] text-[1.4rem]' value={value}/>
-          <input ref={inputRef} type="file" id='uploadBtn'  className='hidden' onChange={uploadFileHandler}/>
-          <Button id='uploadBtn' width={13} height={4.2} >파일 선택</Button>
-          {children}
-        </div>
+    <div className="relative flex gap-[0.8rem]">
+      <input
+        disabled
+        style={{ width: `${inputWidth}rem`, height: `${inputHeight}rem` }}
+        className="rounded border border-[#474E57] bg-neutral-navy-950 px-[1.1rem] text-[1.4rem]"
+        value={value}
+      />
+      <input type="file" id="uploadBtn" className="hidden" onChange={uploadFileHandler} />
+      <Button id="uploadBtn" width={13} height={4.2}>
+        파일 선택
+      </Button>
+      {children}
+    </div>
   )
 }
