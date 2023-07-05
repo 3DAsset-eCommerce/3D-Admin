0139f7e34b31b5fe978b23f81ad4656a28b2d3a0
youngeun <jessicak0828@gmail.com>
2023-07-06 02:19:08 +0900

    feat: :sparkles: cSR 에서 SSG 으로 변경

--- src/components/Table/TableContainer/index.tsx
+++ src/components/Table/TableContainer/index.tsx
@@ -1,26 +1,17 @@
-"use client"
-import React, { useState, ReactNode,Children, cloneElement, ReactElement } from 'react'
+import React, { Children, cloneElement, ReactElement } from 'react'
 
 interface TableContainerProps {
   children: ReactElement[]
 }
 
-export default function TableContainer({children}:TableContainerProps) {
-
-
+export default function TableContainer({ children }: TableContainerProps) {
   return (
-    <ul className='flex flex-col gap-[5rem]'>
-      {
-        Children.map(children,(child,index)=> {
-          return cloneElement(child,{
-            key: index,
-           
-           
-          })
+    <ul className="flex flex-col gap-[5rem]">
+      {Children.map(children, (child, index) => {
+        return cloneElement(child, {
+          key: index,
         })
-      }
+      })}
     </ul>
-
-
   )
 }
