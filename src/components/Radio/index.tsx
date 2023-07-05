f3a1ce635a1c29989827d0764ac5575eb3d1562a
youngeun <jessicak0828@gmail.com>
2023-07-06 02:16:32 +0900

    feat: :sparkles: radio Select 컴포넌트 생성

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,28 +1,32 @@
-"use client"
-import React , {useState,useRef,useContext, ReactNode}from 'react'
-import { VisuallyHidden, useRadio} from 'react-aria';
-import { RadioContext } from './RadioGroup';
-import { RadioActive, RadioInactive } from '../../../public/icons/icons';
+'use client'
+import React, { useState, useRef, useContext, ReactNode } from 'react'
+import { VisuallyHidden, useRadio } from 'react-aria'
+import { RadioContext } from './RadioContainer'
+import { RadioActive, RadioInactive } from '../../../public/icons/icons'
 // RadioGroup is the same as in the previous example
 interface RadioProps {
-  children: ReactNode,
-  value: string,
+  children: ReactNode
+  value: string
 }
 
-
-export default function Radio(props:RadioProps) {
-  const { children, value } = props;
-  const state = useContext(RadioContext);
-  const inputRef = useRef(null);
-  let{ inputProps, isSelected } = useRadio(props, state, inputRef);
+export default function Radio(props: RadioProps) {
+  const { children, value } = props
+  const state = useContext(RadioContext)
+  const inputRef = useRef(null)
+  let { inputProps, isSelected } = useRadio(props, state, inputRef)
   const radioClickHandler = () => {
-    isSelected = true;
+    isSelected = true
   }
   return (
-    <label className='flex gap-[0.8rem] items-center hover:cursor-pointer' onClick={radioClickHandler} >
-      <VisuallyHidden><input {...inputProps} ref={inputRef} className='text-[2.4rem]' /></VisuallyHidden>
-      {isSelected?<RadioActive />:<RadioInactive/>}
-      <span className='text-[1.4rem]'>{children}</span>
+    <label
+      className="flex items-center gap-[0.8rem] hover:cursor-pointer"
+      onClick={radioClickHandler}
+    >
+      <VisuallyHidden>
+        <input {...inputProps} ref={inputRef} className="text-[2.4rem]" />
+      </VisuallyHidden>
+      {isSelected ? <RadioActive /> : <RadioInactive />}
+      <span className="text-[1.4rem]">{children}</span>
     </label>
-  );
+  )
 }
