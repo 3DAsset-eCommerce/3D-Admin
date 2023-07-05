ea67c37ecfb0c8631fa93912d2c090d92c4ec965
youngeun <jessicak0828@gmail.com>
2023-07-06 00:47:45 +0900

    feat: :sparkles: page 경로 수정

---
+++ src/app/assets/search/page.tsx
@@ -0,0 +1,74 @@
+/**
+ * 에셋 조회/수정 페이지
+ */
+import React from 'react'
+import Layout from '@/components/Layout'
+import Header from '@/components/Header/Index'
+import Body from '@/components/Body'
+import TableContainer from '@/components/Table/TableContainer'
+import TableItem from '@/components/Table/TableItem'
+import Button from '@/components/Button'
+import Divider from '@/components/Divider'
+import SelectContainer from '@/components/Select/SelectContainer'
+import AssetsTableContainer from '@/components/AssetsTableContainer'
+
+export default function AssetsPage() {
+  return (
+    <Layout>
+      <Header title="에셋 조회/수정" required={false} isButton={false} />
+      <Body>
+        <TableContainer>
+          <TableItem title="검색어" required={false}>
+            <div className="flex gap-[6.4rem]">
+              <label htmlFor="assetId">
+                <span className="pr-[1.6rem] text-[1.4rem]">상품번호</span>
+                <input
+                  type="text"
+                  id="assetId"
+                  className="rounded border border-neutral-navy-300 bg-neutral-navy-950 px-[2rem] py-[0.9rem] text-[1.4rem]"
+                />
+              </label>
+              <label htmlFor="assetName">
+                <span className="pr-[1.6rem] text-[1.4rem]">상품명</span>
+                <input
+                  type="text"
+                  id="assetName"
+                  className="rounded border border-neutral-navy-300 bg-neutral-navy-950 px-[2rem] py-[0.9rem] text-[1.4rem]"
+                />
+              </label>
+            </div>
+            <div></div>
+          </TableItem>
+          <TableItem title="카테고리" required={false}>
+            <SelectContainer />
+          </TableItem>
+
+          <TableItem title="판매상태" required={false}>
+            {/* <RadioGroup label="saleState">
+                <Radio value="전체">전체</Radio>
+                <Radio value="판매중">판매중</Radio>
+                <Radio value="판매중지">판매중지</Radio>
+              </RadioGroup> */}
+          </TableItem>
+        </TableContainer>
+        <div className="buttonContainer mt-[5rem] flex justify-center gap-[1.6rem]">
+          <Button width={12.5} height={3.6}>
+            검색
+          </Button>
+          <Button width={12.5} height={3.6} backgroundColor="transparent" borderColor="#60AFFF">
+            초기화
+          </Button>
+        </div>
+      </Body>
+      <Divider />
+      <Body>
+        <div>
+          <span>
+            에셋 목록 (총 <span className="text-primary-main">{10}</span>개)
+          </span>
+        </div>
+        <AssetsTableContainer/>
+      </Body>
+    </Layout>
+  )
+}
---
+++ src/app/order/search/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import Header from '@/components/Header/Index'
+import Divider from '@/components/Divider'
+import Body from '@/components/Body'
+import TableContainer from '@/components/Table/TableContainer'
+import TableItem from '@/components/Table/TableItem'
+import RadioContainer from '@/components/Radio/RadioContainer'
+import DateInput from '@/components/Input/DateInput'
+import ListBox from '@/components/Select/ListBox'
+import TextInput from '@/components/Input/TextInput'
+import Button from '@/components/Button'
+import OrderTableContainer from '@/components/OrderTableContainer'
+
+export default function SalesManagementPage() {
+  return (
+    <Layout>
+      <Header title="주문통합검색" required={false} isButton={false} />
+      <Body>
+        <TableContainer>
+          <TableItem title="주문일" required={false}>
+            <RadioContainer
+              type="rectangle"
+              label="orderedDate"
+              array={['오늘', '일주일', '1개월', '3개월', '1년']}
+            />
+            <DateInput />
+          </TableItem>
+          <TableItem title="상세조건" required={false}>
+            <ListBox title="주문번호" array={['주문번호', '상품명', '상품번호', '구매자 이메일']} />
+            <TextInput required={false} width={24} placeholder="" />
+          </TableItem>
+          <div className="buttonContainer mt-[5rem] flex justify-center gap-[1.6rem] text-[1.4rem]">
+            <Button width={12.5} height={3.6}>
+              검색
+            </Button>
+            <Button width={12.5} height={3.6} backgroundColor="transparent" borderColor="#60AFFF">
+              초기화
+            </Button>
+          </div>
+        </TableContainer>
+        <Divider height={0.2} />
+        <OrderTableContainer />
+      </Body>
+    </Layout>
+  )
+}
