f6a0d4c3c26ed8cddbf45d4a2e97f89079a95ded
youngeun <jessicak0828@gmail.com>
2023-07-07 21:28:13 +0900

    feat: :sparkles: input Image(File) 상황별 조건에 따른 기능 구현

--- src/components/Input/ImageUploader.tsx
+++ src/components/Input/ImageUploader.tsx
@@ -1,26 +1,60 @@
 'use client'
 import React, { useState, useRef, ChangeEvent } from 'react'
+import { useDispatch } from 'react-redux'
+import { createThumbnailSrc, createPreviewUrlList } from '@/store/assetSlice'
+import { debounce } from '@/utils/debounce'
 
 interface ImageUploaderProps {
+  type: 'thumbnail' | 'detail'
   required: boolean
   width: number
   height: number
   id: number
 }
 
-export default function ImageUploader({ required, width, height, id }: ImageUploaderProps) {
+export default function ImageUploader({ type, required, width, height, id }: ImageUploaderProps) {
   const [isUploaded, setIsUploaded] = useState<boolean>(false)
   const [url, setUrl] = useState<string>('')
   const inputRef = useRef<HTMLInputElement>(null)
-  const [fileName, setFileName] = useState<string>('')
-  const [fileSize, setFileSize] = useState<number>(0)
+  const [imageName, setImageName] = useState<string>('')
+  const [imageSize, setImageSize] = useState<number>(0)
+  const dispatch = useDispatch()
+  let urlList
   const imageUploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.currentTarget.files && e.currentTarget.files[0]
-    const url = URL.createObjectURL(file)
-    setIsUploaded(true)
-    setUrl(url)
-    setFileName(file.name)
-    setFileSize(file.size)
+    //이미지 파일이 존재할 때,
+    if (e.currentTarget.files !== null) {
+      const image = e.currentTarget.files[0]
+      const imageSize = Number((image.size / 1024 ** 2).toFixed(2))
+      //이미지 파일 사이즈가 10메가바이트 초과면,
+      if (imageSize > 10) {
+        setUrl('')
+        alert('최대 10MB까지 업로드 가능합니다.')
+      } else {
+        // 이미지 파일 사이즈가 10메가바이트 이하면,
+        const url = URL.createObjectURL(image)
+        //이미지 업로드 상태 = 업로드 => X  버튼 나옴
+        setIsUploaded(true)
+        //선택한 이미지 파일 상자에 렌더
+        setUrl(url)
+        //이미지 파일명과 사이즈 상자 밑에 렌더시키기
+        setImageName(image.name)
+        setImageSize(image.size)
+        if (type === 'thumbnail') {
+          //2초 동안 아무 동작 없으면 그제서야 dispatch 하기
+          debounce(() => {
+            dispatch(createThumbnailSrc(url))
+          }, 2000)()
+          console.log('thumbnailRedux')
+        } else if (type === 'detail') {
+          // debounce(() => {
+          //   dispatch(createPreviewUrlList(urlList))
+          // }, 2000)()
+          // console.log('detailImageSrcRedux')
+        }
+      }
+    } else {
+      alert('파일이 존재하지 않습니다.')
+    }
   }
   const imageRemoveHandler = () => {
     setIsUploaded(false)
@@ -30,7 +64,7 @@ export default function ImageUploader({ required, width, height, id }: ImageUplo
     <div className="flex flex-col">
       <div
         style={{ width: `${width}rem`, height: `${height}rem`, backgroundImage: `url(${url})` }}
-        className="relative flex items-center justify-center rounded border border-[#474E57] bg-neutral-navy-950 bg-contain bg-no-repeat px-[2rem]"
+        className="relative flex items-center justify-center rounded border border-[#474E57] bg-neutral-navy-950 bg-cover bg-no-repeat px-[2rem]"
       >
         <label htmlFor={`imageUploadBtn${id}`}>
           <span
@@ -62,9 +96,9 @@ export default function ImageUploader({ required, width, height, id }: ImageUplo
         id={`imageUploadBtn${id}`}
       />
       {isUploaded && (
-        <div className="ml-[0.5rem] mt-[1.2rem]">
-          <p>{fileName}</p>
-          <p>{fileSize / 1024}</p>
+        <div className="ml-[0.5rem] mt-[1.2rem] w-[21.4rem]">
+          <p className="flex flex-wrap">{imageName}</p>
+          <p>{(imageSize / 1024 ** 2).toFixed(2)}MB</p>
         </div>
       )}
     </div>
